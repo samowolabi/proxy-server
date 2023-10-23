61fe75e08d7d42410c99a66d3f4bba7344a594c3
Samuel Owolabi <53657942+Samuel17715@users.noreply.github.com>
2023-10-23 11:38:26 +0100

    Setup HTTP Proxy server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const dotenv = require('dotenv');
+
+//For env File 
+dotenv.config();
+
+const app = express();
+
+// Define the target URL you want to proxy to (the HTTP endpoint).
+const target = process.env.BASE_URL_TARGET || 'http://2.64.32.55';
+
+// Create a proxy middleware that will handle requests to the API.
+const apiProxy = createProxyMiddleware({
+  target,
+  changeOrigin: true, // Needed for changing the host header to the target URL's host
+  secure: false, // Set this to false to avoid SSL certificate verification
+});
+
+// Use the API proxy middleware for requests to the /api route.
+app.use('/api', apiProxy);
+
+// Start the proxy server on a port (e.g., 3001).
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port} and proxying to ${target}`);
+});
